56f2fe1c206b2a18e25653ee6e3b8a32dae8edd2
Kuleshovegor <kuleshovegor2001@gmail.com>
2022-05-27 04:01:45 +0300

    delete updating in initilizer

--- src/main/kotlin/initializers/MyInitializer.kt
+++ src/main/kotlin/initializers/MyInitializer.kt
@@ -2,18 +2,15 @@ package initializers
 
 import com.slack.api.bolt.App
 import com.slack.api.bolt.Initializer
-import models.YouTrackMention
 import org.kodein.di.DI
 import org.kodein.di.instance
 import service.AppHomeService
 import service.UnansweredMessageService
-import service.YouTrackCommentService
 import java.util.*
 import kotlin.concurrent.timerTask
 
 class MyInitializer(di: DI) : Initializer {
     private val unansweredMessageService: UnansweredMessageService by di.instance()
-    private val youTrackCommentService: YouTrackCommentService by di.instance()
     private val appHomeService: AppHomeService by di.instance()
 
     override fun accept(initApp: App?) {
@@ -23,6 +20,5 @@ class MyInitializer(di: DI) : Initializer {
         timer.schedule(timerTask { appHomeService.publishAll() }, 0, 3600_000)
 
         unansweredMessageService.updateAllMessages()
-//        youTrackCommentService.updateAll()
     }
 }
